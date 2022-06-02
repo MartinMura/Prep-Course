e7a15cc85020cc1b3a712169eeeaf908021124f7
MartinMura <martinalejandropm@gmail.com>
2022-06-02 15:41:27 -0300

    js homework terminada

--- 04-JS-III/homework/homework.js
+++ 04-JS-III/homework/homework.js
@@ -184,6 +184,15 @@ function todosIguales(arreglo) {
   //Escriba la función todosIguales, que indique si todos los elementos de un arreglo son iguales:
   //retornar true, caso contrario retornar false.
   //Escribe tu código aquí
+  for(let i = 0; i < arreglo.length; i++){
+    
+    if(arreglo[0] == arreglo[i+1]){
+      return true
+    } else {
+      return false
+    }
+    
+  }
   
 
   
@@ -213,7 +222,7 @@ function mayorACien(array) {
   //La función recibe un array con enteros entre 0 y 200. Recorrer el array y guardar en un nuevo array sólo los
   //valores mayores a 100 (no incluye el 100). Finalmente devolver el nuevo array.
   // Tu código:
-  let mayores = array.filter(i => i > 100);
+  let mayores = array.filter(numero => numero > 100);
   return mayores
 }
 
@@ -226,6 +235,19 @@ function breakStatement(numero) {
   //devolver: "Se interrumpió la ejecución"
   //Pista: usá el statement 'break'
   // Tu código:
+  let nuevoArray = [];
+  let suma = numero
+  for(let i = 0; i < 10; i++){
+
+    if(suma === i){
+      return "Se interrumpió la ejecución"
+    } 
+    suma = suma + 2;
+    nuevoArray.push(suma)
+    
+  }
+  return nuevoArray
+  
   
 
   
@@ -239,6 +261,18 @@ function continueStatement(numero) {
   //Cuando el número de iteraciones alcance el valor 5, no se suma en ese caso y se continua con la siguiente iteración
   //Pista: usá el statement 'continue'
   // Tu código:
+  let suma = numero
+  let nuevoArray = []
+  for(let i = 0; i < 10; i++){
+    
+    if(i === 5){
+      continue
+    }
+    suma = suma + 2
+    nuevoArray.push(suma)
+    
+  }
+  return nuevoArray
 }
 
 
